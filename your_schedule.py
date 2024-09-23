151e2c96d571731c70c3c8b7d3f851f94f38f563
FearlessYak5088 <sunfan2266@gmail.com>
2024-09-23 10:43:14 -0400

    Create your_schedule.py

---
+++ your_schedule.py
@@ -0,0 +1,20 @@
+# Your Schedule
+
+# Variable names
+course1 = "Chemistry"
+course2 = "Computer Science"
+course3 = "Functions"
+course4 = "Psychology"
+teacher1 = "Mr. Cusimano"
+teacher2 = "Mr. Gallo"
+teacher3 = "Mr. Bisogno"
+teacher4 = "Mrs. Mclelland"
+
+print(f"""
++{"-" * 60}+
+|1| {course1:>26} | {teacher1:>27} |
+|2| {course2:>26} | {teacher2:>27} |
+|3| {course3:>26} | {teacher3:>27} |
+|4| {course4:>26} | {teacher4:>27} |
++{"-" * 60}+
+""")
