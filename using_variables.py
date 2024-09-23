0767a0ae6b4851e84de266d66a291d183fbd9d2c
FearlessYak5088 <sunfan2266@gmail.com>
2024-09-22 21:56:58 -0400

    Create using_variables.py

---
+++ using_variables.py
@@ -0,0 +1,7 @@
+room_number = 113
+value_of_e = 2.71828
+learning_subject = "Computer Science"
+
+print(f"This is room #{room_number}")
+print(f"e is close to {value_of_e}")
+print(f"I am learning a bit about {learning_subject}")
