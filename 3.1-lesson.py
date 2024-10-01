dfa21b722218f5989aff9b5e523b894988aa80a9
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-01 10:25:38 -0400

    Update 3.1-lesson.py

--- 3.1-lesson.py
+++ 3.1-lesson.py
@@ -87,7 +87,16 @@ if robot_location == goal_location and have_ball is True:
 
 
 # # # How Old Are You?
-
+name = input("What is your name? ")
+age = int(input(f"Ok {name}, how old are you? "))
+if age < 16: 
+    print("You can't drive.")
+if age < 18:
+    print("You can't vote.")
+if age < 21:
+    print("You can't rent a car.")
+if age > 21:
+    print("You can do anything legal.")
 
 
 
