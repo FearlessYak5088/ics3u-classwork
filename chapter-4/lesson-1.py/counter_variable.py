70d029b5ad10d8ab8b68f562ee023d84ce9a10b6
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-07 10:59:48 -0400

    Create counter_variable.py

---
+++ chapter-4/lesson-1.py/counter_variable.py
@@ -0,0 +1,40 @@
+# Create a loop that will count from:
+
+# 0 to 10 by 1.
+n = 0
+while n < 10:
+    print(f"{n + 1}")
+    n += 1
+
+# 10 to 0 by 1.
+# Start at 11 because it is 1 more than 10
+n = 11
+while n > 0:
+  print(f"{n - 1}")
+  n -= 1
+  
+# from 491 to 586 by 5.
+# Start at 486 because it is 5 less than 491
+n = 486
+while n < 586:
+  print(f"{n + 5}")
+  n += 5
+  
+# from 124 to 810 by 14.
+# Start at 110 because it is 14 less than 124
+n = 110
+while n < 810:
+  print(f"{n + 14}")
+  n += 14
+  
+# from -233 to -113 by 4.
+n = -237
+while n < -113:
+  print(f"{n + 4}")
+  n += 4
+  
+# from 215 to 300 by 5.
+# from -310 to -94 by 8.
+# from -476 to -410 by 3.
+# from -327 to -147 by 9.
+# from 484 to 1024 by 18.
