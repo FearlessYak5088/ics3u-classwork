b7869299fe89c929b18b37c4565b10e117f2c0d2
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-08 18:42:15 -0400

    Create break_and_continue.py

---
+++ chapter-4/lesson-4.py/break_and_continue.py
@@ -0,0 +1,4 @@
+# Create a loop that counts from 0 to 10, but skip the number 7.
+# Create a loop that will add the numbers from 5 to 20 but not any multiples of 3. Use modulus.
+# Ask the user for a starting number and an ending number. The program will get the sum of all the numbers from the start to the end (using a loop). However, our program is a bit of a diva: the program will stop summing the numbers if it encounters a 13 or a 31. It will still output the sum up to that point.
+# Create an infinite loop using while True:. In that loop ask the user for a word. For each word they enter, output the following "Your word: {word}". If ever they enter the word "stop" the program will break out of the loop. Finally the program will output "Goodbye!".
