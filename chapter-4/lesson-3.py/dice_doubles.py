99e6dad67746a16d8f3a0f1739e4537ae051eeb4
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-08 11:02:20 -0400

    Create dice_doubles.py

---
+++ chapter-4/lesson-3.py/dice_doubles.py
@@ -0,0 +1,11 @@
+import random
+
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+
+while dice1 != dice2:
+  print(f"\nRoll #1: {dice1}\nRoll #2: {dice2}\nThe total is {dice1 + dice2}!")
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+if dice1 == dice2:
+  print(f"\nRoll #1: {dice1}\nRoll #2: {dice2}\nThe total is {dice1 + dice2}! Both dice are the same number.")
