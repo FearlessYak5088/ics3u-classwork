87701fc9aa38594cafb89fc4361d49d60e481644
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-08 10:13:36 -0400

    Update counter_variable.py

--- chapter-4/lesson-1.py/counter_variable.py
+++ chapter-4/lesson-1.py/counter_variable.py
@@ -7,34 +7,55 @@ while n < 10:
     n += 1
 
 # 10 to 0 by 1.
-# Start at 11 because it is 1 more than 10
-n = 11
+n = 10 + 1
 while n > 0:
   print(f"{n - 1}")
   n -= 1
   
 # from 491 to 586 by 5.
-# Start at 486 because it is 5 less than 491
-n = 486
+n = 491 - 5
 while n < 586:
   print(f"{n + 5}")
   n += 5
   
 # from 124 to 810 by 14.
-# Start at 110 because it is 14 less than 124
-n = 110
+n = 124 - 14
 while n < 810:
   print(f"{n + 14}")
   n += 14
   
 # from -233 to -113 by 4.
-n = -237
+n = -233 - 4
 while n < -113:
   print(f"{n + 4}")
   n += 4
   
 # from 215 to 300 by 5.
+n = 215 - 5
+while n < 300:
+    print(f"{n + 5}")
+    n += 5
+
 # from -310 to -94 by 8.
+n = -310 - 8
+while n < -94:
+    print(f"{n + 8}")
+    n += 8
+
 # from -476 to -410 by 3.
+n = -476 - 3
+while n < -410:
+    print(f"{n + 3}")
+    n += 3
+
 # from -327 to -147 by 9.
+n = -327 - 9
+while n < -147:
+    print(f"{n + 9}")
+    n += 9 
+
 # from 484 to 1024 by 18.
+n = 484 - 18
+while n < 1024:
+    print(f"{n + 18}")
+    n += 18
