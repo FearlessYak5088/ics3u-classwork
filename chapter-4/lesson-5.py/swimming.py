da392af78587df7a9d8613b711d50854b896981f
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-10 10:57:44 -0400

    Create swimming.py

---
+++ chapter-4/lesson-5.py/swimming.py
@@ -0,0 +1,19 @@
+# Run the program, and type in 80.5 for the current water temperature. Do Goofus and Gallant swim for the same amount of time?
+# Put your answer in a comment.
+# Both Goofus and Gallant swim the same amount of time (4 min)
+
+# Run the program again, but this time enter 78 for the starting temperature. What changes?
+# Gallant does not swim, and Goofus swims for 1 min. 
+
+# Does Gallant check the water temperature first, or does he just dive right in?
+# Gallant checks the temperature first
+
+# What about Goofus? Does he check the water temperature first or just dive in?
+# Goofus dives in
+
+# What is the difference between the first while loop and the second while loop? What do you suppose the keyword break does in the second loop?
+# The first while loop will work if the current temperature is greater than or equal to the minimum temperature, 
+# The second while loop will work no matter what. The break keyword in the second loop stops the loop when the current temperature is less than the minimum temperature.
+
+# One of these loops is sometimes called a “pre-test loop”, and the other is called a “post-test loop”. Which one is which?
+# The first one is a pre-test loop(it checks the condition before running the loop), and the second one is a post-test loop (it checks the condition after running the loop once).
