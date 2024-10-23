746b05a19b8ee865ab415f4d0fae033c1c1d754c
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-23 14:11:32 -0400

    Create list0.py

---
+++ chapter-5/lesson-1/list0.py
@@ -0,0 +1,26 @@
+
+# Create an empty list. Append to the array the number -113 ten times using the list.append() method. 
+# Then display the contents of the list on the screen. One element at a time.
+# Do not use a loop. Also, do not use any variable for the index; you must use literal numbers to refer to each slot.
+  
+list = []
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+print(f"slot 0 contains {list[0]}")
+print(f"slot 1 contains {list[1]}")
+print(f"slot 2 contains {list[2]}")
+print(f"slot 3 contains {list[3]}")
+print(f"slot 4 contains {list[4]}")
+print(f"slot 5 contains {list[5]}")
+print(f"slot 6 contains {list[6]}")
+print(f"slot 7 contains {list[7]}")
+print(f"slot 8 contains {list[8]}")
+print(f"slot 9 contains {list[9]}")
