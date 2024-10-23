7ad5eb6f507323857a7af6cf9fa53afdc63e6ed6
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-23 14:21:27 -0400

    Create list1.py

---
+++ chapter-5/lesson-1/list1.py
@@ -0,0 +1,13 @@
+list = []
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+list.append(-113)
+for i in range(len(list)):
+    print(f"Slot {i} contains a {list[i]}")
