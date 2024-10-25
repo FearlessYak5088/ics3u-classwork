e3dad53d084d5b1d71d6542c8d46f54e950717c7
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-25 15:10:35 -0400

    Create animal_name.py

---
+++ chapter-5/lesson-4/animal_name.py
@@ -0,0 +1,9 @@
+# Create a program that will split a string of animal names into a list. 
+# E.g., "monkey bat cat dog". Use a loop to print out all the animal names one at a time.
+
+animals_string = "monkey bat cat dog"
+
+animals_list = animals_string.split(" ")
+
+for animals_string in animals_list:
+    print(animals_string)
