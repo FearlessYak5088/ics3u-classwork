460a2b853b337f5be9479e8514b29b3c9fff879b
FearlessYak5088 <sunfan2266@gmail.com>
2024-10-31 12:36:23 -0400

    Update landscape.py

--- pygame/landscape.py
+++ pygame/landscape.py
@@ -1,4 +1,4 @@
-# # # Landscape Assignment
+# # # Landscape
 
 import pygame
 import sys
@@ -12,23 +12,23 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Landscape by Fan")
 
 # colours
-blue = [0, 0, 139]
-blue2 = [135, 206, 250]
+blue = (0, 0, 139)
+blue2 = (135, 206, 250)
 green = (1, 70, 42)
 gray = (51, 51, 51)
 gray2 = (75, 75, 75)
 orange = (200, 100, 0)
-orange2 = (255,140,0)
 brown = (92, 64, 51)
 white = (144, 144, 144)
 white2 = (200, 200, 200)
 black = (0, 0, 0)
+orange2 = (255,140,0)
 
-moon_x = 120
-moon_y = 125
+moon_x = 0
+moon_y = 155
 sun_x = 120
 sun_y = 125
-moon_active = True
+sun_active = True
 
 # loop
 while True:
@@ -38,10 +38,10 @@ while True:
             sys.exit()
 
     # sky
-    if moon_active:
-        screen.fill(blue)
-    elif moon_active == False:
+    if sun_active:
         screen.fill(blue2)
+    elif sun_active == False:
+        screen.fill(blue)
 
     # ground
     pygame.draw.rect(screen, green, (0, 400, width, height - 400))
@@ -63,36 +63,41 @@ while True:
     pygame.draw.line(screen, black, (525, 380), (525, 460), 2)
     pygame.draw.line(screen, black, (485, 420), (565, 420), 2)
 
-    if moon_active:
-        # Draw the moon
-        pygame.draw.circle(screen, white, (moon_x, moon_y), 60)
-
-        # Moon texture
-        pygame.draw.circle(screen, gray2, (moon_x - 20, moon_y + 20), 20)
-        pygame.draw.circle(screen, gray2, (moon_x - 10, moon_y - 20), 12)
-
-        # Move the moon across the sky
-        if moon_x <= width // 2:  # Move moon until it is off the screen
-            moon_x += 0.05
-            moon_y -= 0.01
-        elif moon_x < width + 60:
-            moon_x += 0.05
-            moon_y += 0.01
-        else:
-            moon_active = False  # Deactivate the moon when it goes off screen
-    else:
-        # Draw the sun
+    if sun_active:
+        # draw the sun
         pygame.draw.circle(screen, orange2, (sun_x, sun_y), 60)
         
-
-        # Move the sun up
+        # move the sun up
         if sun_x <= width // 2:
             sun_x += 0.05
             sun_y -= 0.01
-        else:
+        elif sun_x < width + 65:
             sun_x += 0.05
             sun_y += 0.01
+        else:
+            sun_active = False
+
+    else:
+        # draw the moon
+        pygame.draw.circle(screen, white, (moon_x, moon_y), 60)
+
+        # moon texture
+        pygame.draw.circle(screen, gray2, (moon_x - 20, moon_y + 20), 20)
+        pygame.draw.circle(screen, gray2, (moon_x - 10, moon_y - 20), 12)
+
+        # move the moon
+        if moon_x <= width // 2:  
+            moon_x += 0.05
+            moon_y -= 0.01
+        else:
+            moon_x += 0.05
+            moon_y += 0.01
+            
 
     # update the display
     pygame.display.flip()
+
+
+
+
 
