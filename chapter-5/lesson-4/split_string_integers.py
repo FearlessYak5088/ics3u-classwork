4c97e392deae6266c1a5dd29e48f137a5be19aea
FearlessYak5088 <sunfan2266@gmail.com>
2024-11-07 17:17:08 -0500

    Update split_string_integers.py

--- chapter-5/lesson-4/split_string_integers.py
+++ chapter-5/lesson-4/split_string_integers.py
@@ -1,1 +1,13 @@
+# Create a program that will split a string of integers separated by comma-space. 
+# For example "65, 75, 32, 22". Be sure to convert the numbers to integers. Print out all the index locations matched with the values.
 
+string = "65, 75, 32, 22"
+
+list = []
+
+string_int = string.split(" ")
+string_int = string.split (",")
+
+for string in string_int:
+    list.append(int(string))
+print(list)
