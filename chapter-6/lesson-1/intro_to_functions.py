9cd457c8c0422c91695548ac894ce6e79532bb03
FearlessYak5088 <sunfan2266@gmail.com>
2024-11-19 13:16:13 -0500

    Create intro_to_functions.py

---
+++ chapter-6/lesson-1/intro_to_functions.py
@@ -0,0 +1,13 @@
+# Hello, Name!
+def main():
+    name = input("Enter your name: ")
+
+    if name == "":
+        name = "User"
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    main()
+
+# Area Calculator
+
