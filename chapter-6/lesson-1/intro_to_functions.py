64f723abdf7df810a377977ab3b6bc0d1f028e31
FearlessYak5088 <sunfan2266@gmail.com>
2024-11-22 10:32:12 -0500

    Update intro_to_functions.py

--- chapter-6/lesson-1/intro_to_functions.py
+++ chapter-6/lesson-1/intro_to_functions.py
@@ -10,4 +10,27 @@ if __name__ == "__main__":
     main()
 
 # Area Calculator
+def greet():
+    print("Welcome to the awesome area calculator!")
+    print()
 
+if __name__ == "__main__":
+    greet()
+    
+length = int(input("Enter a length: "))
+width = int(input("Enter a width: "))
+print()
+
+area = length * width
+
+print(f"The area is {area} units squared")
+
+# Ticket Prices
+TICKET_PRICE = 13.95
+def main():
+    num_tickets = int(input("How many tickets would you like? "))
+    total_price = num_tickets * TICKET_PRICE
+    print(f"The total price is {total_price}")
+
+if __name__ == "__main__":
+    main()
