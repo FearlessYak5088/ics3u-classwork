e4aa1bdab48747c5da84f2e3b6b16c1b3978320d
FearlessYak5088 <sunfan2266@gmail.com>
2024-11-25 10:39:02 -0500

    Create heron's_formula.py

---
+++ chapter-6/lesson-3/heron's_formula.py
@@ -0,0 +1,24 @@
+# 1. Run both files (herons_formula.py and herons_formula_no_function.py). Do they both produce the same output? 
+
+# Yes
+
+# 2. How many lines long is herons_formula_no_function.py? 
+# How many lines long is herons_formula.py if you don’t count the 10 lines of comments inside the triangle_area() function?
+
+# herons_formula_no_function.py is 49 lines
+# herons_formula.py is 32 lines
+
+# 3. There is a bug in the formula for both files. When (a+b+c) is an odd number, floor-dividing by 2 throws away the .5. 
+# Fix both files so that instead of (a+b+c) // 2 you have (a+b+c) / 2 everywhere it occurs. 
+# Was it easier to fix the file that used a function, or the one that didn’t use a function? Answer in a comment.
+
+# The function was easier because you just had to switch it once
+
+# 4.Add one more test to both files: find the area of a triangle with sides 9, 9, and 9. 
+# Was it difficult to add to the file that used a function? 
+# Answer in a comment on the line below where you added the new function call.
+
+# No, the function is easier because you only change one line
+
+
+
